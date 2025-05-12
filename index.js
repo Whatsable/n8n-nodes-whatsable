f322a6aea2bd8bcc5650088c5148d7947c93c4c2
Younus Ali Akash <younusaliakash8@gmail.com>
2025-05-13 03:35:00 +0600

    feat: add basic trigger node for notifyer

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const { WhatsAble } = require('./dist/nodes/WhatsAble/WhatsAble.node');
+const { WhatsAbleNotifyerSystem } = require('./dist/nodes/WhatsAble/WhatsAbleNotifyerSystem.node');
 
 module.exports = {
   nodes: [
     WhatsAble,
+    WhatsAbleNotifyerSystem,
   ],
 };
---
+++ nodes/WhatsAble/WhatsAbleNotifyerSystem.node.ts
@@ -0,0 +1,88 @@
+import {
+	INodeType,
+	INodeTypeDescription,
+	IWebhookFunctions,
+	IWebhookResponseData,
+} from 'n8n-workflow';
+
+export class WhatsAbleNotifyerSystem implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'WhatsAble Notifyer System',
+		name: 'whatsAbleNotifyerSystem',
+		icon: 'file:whatsable.svg',
+		group: ['trigger'],
+		version: 1,
+		subtitle: '={{$parameter["httpMethod"] + ": " + $parameter["path"]}}',
+		description: 'Incoming Message From recipient',
+		defaults: {
+			name: 'WhatsAble Notifyer System',
+		},
+		inputs: ['main'],
+		outputs: ['main'],
+		credentials: [
+			{
+				name: 'whatsAbleApi',
+				required: true,
+			},
+		],
+		webhooks: [
+			{
+				name: 'default',
+				httpMethod: '={{$parameter["httpMethod"]}}',
+				responseMode: '={{$parameter["responseMode"]}}',
+				path: '={{$parameter["path"]}}',
+				isFullPath: false,
+			},
+		],
+		properties: [
+			{
+				displayName: 'HTTP Method',
+				name: 'httpMethod',
+				type: 'options',
+				options: [
+					{
+						name: 'POST',
+						value: 'POST',
+					},
+				],
+				default: 'POST',
+				description: 'The HTTP method to listen to',
+			},
+			{
+				displayName: 'Path',
+				name: 'path',
+				type: 'string',
+				default: '',
+				placeholder: 'webhook-path',
+				required: true,
+				description: 'The path to listen to',
+			},
+			{
+				displayName: 'Respond',
+				name: 'responseMode',
+				type: 'options',
+				options: [
+					{
+						name: 'Immediately',
+						value: 'onReceived',
+						description: 'As soon as the request is received',
+					},
+					{
+						name: 'When Last Node Finishes',
+						value: 'lastNode',
+						description: 'After the last node in the workflow finishes executing',
+					},
+				],
+				default: 'onReceived',
+				description: 'When to respond to the webhook',
+			},
+		],
+	};
+
+	// The function to execute when the webhook gets called
+	async webhook(this: IWebhookFunctions): Promise<IWebhookResponseData> {
+		return {
+			workflowData: [this.helpers.returnJsonArray(this.getRequestObject().body)],
+		};
+	}
+} 
